fb30d697ced5637efff1a3a2164fa59f56b1acae
Alaa Nassir <alaanassir89@gmail.com>
2026-05-23 14:15:25 +0200

    ...

--- expense-tracker/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import { useAuth } from "../contexts/AuthContext"
-import { Navigate } from "react-router-dom"
-
-
-export const ProtectedRoute = ({children}) => {
-    const {user} = useAuth()
-    if(user) {return children} else {return <Navigate to="/login" />}
-}
